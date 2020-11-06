2aa55cd731d324a4aa478928b2774b8ff4bfb5ed
rmhoward <rmhoward@seas.upenn.edu>
2020-11-06 14:13:09 +0500

    Methods added.

--- src/battleship/EmptySea.java
+++ src/battleship/EmptySea.java
@@ -1,4 +1,25 @@
 package battleship;
 
 public class EmptySea extends Ship {
+
+    public EmptySea() {
+    }
+
+    @Override
+    boolean shootAt(int row, int column) {
+    }
+
+    @Override
+    boolean isSunk() {
+    }
+
+    @Override
+    public String toString() {
+    }
+
+    @Override
+    public String getShipType() {
+    }
+
+
 }
