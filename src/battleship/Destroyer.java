98127fcd1f7960cb534dc2fc7b100ef07add8802
rmhoward <rmhoward@seas.upenn.edu>
2020-11-06 14:48:12 +0500

    Methods added.

--- src/battleship/Destroyer.java
+++ src/battleship/Destroyer.java
@@ -1,4 +1,13 @@
 package battleship;
 
 public class Destroyer extends Ship {
+
+    public Destroyer {
+        this.length = 2;
+    }
+
+    public String getShipType() {
+        return "Destroyer";
+    }
 }
+
