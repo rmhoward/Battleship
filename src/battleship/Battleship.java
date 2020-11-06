7ab1e093300d64f5279a138f50ac733cf0a8fb91
rmhoward <rmhoward@seas.upenn.edu>
2020-11-06 14:47:08 +0500

    Methods added.

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -1,4 +1,13 @@
 package battleship;
 
 public class Battleship extends Ship {
+
+    public Battleship {
+        this.length = 4;
+    }
+
+    public String getShipType() {
+        return "Battleship";
+    }
+
 }
