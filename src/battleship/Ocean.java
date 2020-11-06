27a9cb25605e0f4ce17e3ec49e8530a06a5cff8b
rmhoward <rmhoward@seas.upenn.edu>
2020-11-06 14:15:59 +0500

    Methods added.

--- src/battleship/Ocean.java
+++ src/battleship/Ocean.java
@@ -1,4 +1,40 @@
 package battleship;
 
 public class Ocean {
+
+    private Ship[][]ships = new Ship[10][10];
+
+    private int shotsFired;
+
+    private int hitCount;
+
+    private int shipsSunk;
+
+    public Ocean() {
+    }
+
+    void placeAllShipsRandomly() {
+    }
+
+    boolean isOccupied(int row, int column) {
+    }
+
+    boolean shootAt(int row, int column) {
+    }
+
+    int getShotsFired() {
+    }
+
+    int getHitCount() {
+    }
+
+    int getShipsSunk() {
+    }
+
+    Ship[][] getShipArray() {
+    }
+
+    void print() {
+    }
+
 }
