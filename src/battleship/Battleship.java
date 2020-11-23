bcd31847064fa5c84f25d2e6105f4ca2f1ec674a
Christian <Christian@DESKTOP-JUFM6E7.attlocal.net>
2020-11-23 05:54:45 -0500

    Updated Javadoc

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -4,8 +4,9 @@ package battleship;
 public class Battleship extends Ship {
 
 	/**
-	 * The shiplength is derived from the ship class. 
-	 * Battleship has a shipLengh of 4.
+	 * The shipLength is derived from the ship class. 
+	 * Battleship has a shipLength of 4.
+	 * The bow will always face the right of the gameboard
 	 */
 	private static final int shipLength = 4;
 	
