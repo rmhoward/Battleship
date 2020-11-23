3b8a988726c64d52bf0fef7e942d034e86de8367
Christian <Christian@DESKTOP-JUFM6E7.attlocal.net>
2020-11-23 05:46:48 -0500

    Test Javadoc

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -2,6 +2,10 @@ package battleship;
 
 public class Battleship extends Ship {
 
+	/**
+	 * The shiplength is derived from the ship class. 
+	 * Battleship has a shipLengh of 4.
+	 */
 	private static final int shipLength = 4;
 	
     public Battleship(int length) {
