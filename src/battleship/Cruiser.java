e81a1389fba5d5226e178c6aca0384db94d89f5c
Christian <Christian@DESKTOP-JUFM6E7.attlocal.net>
2020-11-23 06:05:53 -0500

    Updated Javadoc

--- src/battleship/Cruiser.java
+++ src/battleship/Cruiser.java
@@ -5,7 +5,7 @@ public class Cruiser extends Ship {
 	/**
 	 * The shipLength is derived from the ship class. 
 	 * Cruiser has a shipLength of 3.
-	 * The bow will always face the right of the gameboard
+	 * The bow will always face the right of the gameboard.
 	 */
 	private static final int shipLength = 3;
 
